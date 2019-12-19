9a71956a87a3e761606e3f75d6e4ff29a7abbbb5
F Pucher <franz.pucher@gmail.com>
2019-12-19 17:43:35 +0100

    Update ranger.py

--- ros/src/grove_ultrasonic_ranger/src/ranger.py
+++ ros/src/grove_ultrasonic_ranger/src/ranger.py
@@ -2,7 +2,7 @@
 
 from math import pi
 
-from ultrasonic_ranger import UltrasonicRanger # <1>
+from grove_ultrasonic_ranger import GroveUltrasonicRanger # <1>
 
 import rospy
 import tf
@@ -10,23 +10,16 @@ import tf
 from sensor_msgs.msg import Range # <2>
 
 
-def cm2m(distance):  # <3>
-    q = tf.transformations.quaternion_from_euler(0, 0, angle)
-    return Quaternion(*q)
-
-
 if __name__ == '__main__':
-    sensor = UltrasonicRanger()  # <4>
+    sensor = GroveUltrasonicRanger()  # <4>
 
-    rospy.init_node('ultrasonic_sensor')
+    rospy.init_node('ranger')
 
     pub = rospy.Publisher('distance', Range, queue_size=10)
 
     rate = rospy.Rate(10.0)  # <5>
     while not rospy.is_shutdown():  # <6>
-        distance = measurementInCM()
-
-        distance = cm2m(distance)
+        distance = sensor.measureDistance()
 
         pub.publish(distance)
 
