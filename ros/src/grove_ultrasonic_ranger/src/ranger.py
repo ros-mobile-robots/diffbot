cb18a26a8c4f406a4299513f73636f406c64138d
fjp <franz.pucher@gmail.com>
2019-12-19 17:34:42 +0000

    uses sensor_msg/Ranger to publish range

--- ros/src/grove_ultrasonic_ranger/src/ranger.py
+++ ros/src/grove_ultrasonic_ranger/src/ranger.py
@@ -16,11 +16,27 @@ if __name__ == '__main__':
     rospy.init_node('ranger')
 
     pub = rospy.Publisher('distance', Range, queue_size=10)
+    
+    # create default message of type sensor_msg/Range with default values
+    # http://docs.ros.org/melodic/api/sensor_msgs/html/msg/Range.html
+    msg = Range()
+    msg.header.frame_id = "ranger_distance"
+    # http://wiki.seeedstudio.com/Grove-Ultrasonic_Ranger/#specification
+    msg.field_of_view = 15
+    msg.min_range = 0.02 # m
+    msg.max_range = 3.5  # m
 
     rate = rospy.Rate(10.0)  # <5>
     while not rospy.is_shutdown():  # <6>
-        distance = sensor.measureDistance()
-
-        pub.publish(distance)
+        range = sensor.measureDistance()
+        
+        # update time stamp of default message
+        msg.header.stamp = rospy.Time.now()
+        
+        # update measured distance to obstacle
+        msg.range = range
+        
+        # publish the updated message
+        pub.publish(msg)
 
         rate.sleep()
