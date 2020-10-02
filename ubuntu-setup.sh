d0fe1276644f41e2e423f968aa17de84eb994230
Franz <franz.pucher@gmail.com>
2020-10-02 21:44:22 +0200

    Update ubuntu-setup.sh to ROS noetic

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -9,11 +9,12 @@ sudo apt install i2c-tools
 # dtparam=i2c0=on
 sudo sed -i '/dtparam=i2c0=on/s/^#//g' /boot/firmware/config.txt
 
-# ROS Melodic setup
+# ROS Noetic setup
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt install ros-noetic-desktop-full
+
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
