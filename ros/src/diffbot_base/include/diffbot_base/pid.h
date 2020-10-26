41f7ce88ca3edca495b682f1d0c7aee9b45db945
Franz Pucher <franz.pucher@gmail.com>
2020-10-26 12:05:41 +0100

    implement basic PID controller

--- ros/src/diffbot_base/include/diffbot_base/pid.h
+++ ros/src/diffbot_base/include/diffbot_base/pid.h
@@ -5,20 +5,25 @@ namespace diffbot_base
     class PID
     {
     public:
-        PID(double kp, double ki, double kd, double setpoint, double output_min, double output_max);
-        double operator()(double measured_value, double setpoint);
-        void setParameters(double kp, double ki, double kd);
+        PID(double kP, double kI, double kD, double setpoint, double output_min, double output_max);
+        PID(double kF, double kP, double kI, double kD, double setpoint, double output_min, double output_max);
+        double operator()(const double &measured_value, const double &setpoint, const double &dt);
+        void setParameters(double kP, double kI, double kD);
+        void setOutputLimits(double output_min, double output_max);
 
     private:
-        double kp_;
-        double ki_;
-        double kd_;
-        double min_val_;
-        double max_val_;
+        double kF_;
+        double kP_;
+        double kI_;
+        double kD_;
+        double setpoint_;
+        double output_min_;
+        double output_max_;
 
+        double proportional_;
         double integral_;
         double derivative_;
-        double prev_error_;
+        double last_error_;
     };
 
 }
---
+++ ros/src/diffbot_base/src/pid.cpp
@@ -0,0 +1,107 @@
+
+#include <diffbot_base/pid.h>
+
+namespace diffbot_base
+{
+
+    PID::PID(double kP, double kI, double kD, double setpoint, double output_min, double output_max)
+    {
+        kF_ = 0.0;
+        kP_ = kP;
+        kI_ = kI;
+        kD_ = kD;
+
+        setpoint_ = setpoint;
+
+        output_min_ = output_min;
+        output_max_ = output_max;
+
+        proportional_ = 0.0;
+        integral_ = 0.0;
+        derivative_ = 0.0;
+    }
+
+    PID::PID(double kF, double kP, double kI, double kD, double setpoint, double output_min, double output_max)
+    {
+        kF_ = kF;
+        kP_ = kP;
+        kI_ = kI;
+        kD_ = kD;
+
+        setpoint_ = setpoint;
+
+        output_min_ = output_min;
+        output_max_ = output_max;
+
+        proportional_ = 0.0;
+        integral_ = 0.0;
+        derivative_ = 0.0;
+    }
+
+    double PID::operator()(const double &measured_value, const double &setpoint, const double &dt)
+    {
+        if (dt <= 0)
+        {
+            throw std::domain_error("dt must be greater than zero.");
+        }
+
+        setpoint_ = setpoint;
+        process_value_ = measured_value;
+
+        // Feed forward term
+
+        // Compute error terms
+        double error = setpoint_ - process_value_;
+        double delta_error = error - last_error_;
+
+        // Compute the proportional term
+        proportional_ = error;
+
+        // Compute integral term
+        integral_ += error * dt;
+        // TODO: avoid integral windup
+        //integral_ = clamp(integral_, output_min_, output_max_)  
+
+        // Compute derivative term
+        derivative_ = delta_error / dt;
+
+        // Compute final output
+        output = kF * setpoint + kP_ * proportional_ + kI_ * integral_ + kD_ * derivative_;
+        //output = clamp(output, output_min_, output_max_)
+
+        // Keep track of state
+        last_error = error;
+
+        return output
+    }
+
+
+    void PID::setParameters(double kP, double kI, double kD)
+    {
+        kP_ = kP;
+        kI_ = kI;
+        kD_ = kD;
+    }
+
+    void PID::setOutputLimits(double output_min, double output_max)
+    {
+        output_min_ = output_min;
+        output_max_ = output_max;
+    }
+
+
+    double PID::clamp(value, lower_limit, upper_limit)
+    {
+        if (value > upper_limit)
+        {
+            return upper_limit;
+        }
+        else if (value < lower_limit)
+        {
+            return lower
+        }
+
+        return value
+    }
+
+}
