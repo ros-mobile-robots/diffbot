2d7db9e75039cbc01a97222cdfd9ac2a333596c1
Franz Pucher <franz.pucher@gmail.com>
2021-05-30 22:50:19 +0200

    add adafruit_feather_wing motor controller class

---
+++ diffbot_base/scripts/base_controller/lib/motor_controllers/adafruit_feather_wing.cpp
@@ -0,0 +1,36 @@
+#include "adafruit_feather_wing/adafruit_feather_wing.h"
+
+
+AdafruitMotorController::AdafruitMotorController(uint8_t motor_num, uint8_t addr)
+{
+    motor_driver_ = Adafruit_MotorShield(addr);
+    // Select which 'port' (motor pin) M1, M2, M3 or M4.
+    pMotor_ = motor_driver_.getMotor(motor_num);
+}
+
+void AdafruitMotorController::begin(uint16_t freq)
+{
+    motor_driver_.begin(freq);
+}
+
+void AdafruitMotorController::setSpeed(int value)
+{
+    if (value > 0)
+    {
+        pMotor_->run(FORWARD);
+    }
+    else if (value < 0)
+    {
+        pMotor_->run(BACKWARD);
+        // AdafruAdafruit_MotorShield requires uint8 values from 0 to 255
+        // Make sure to convert the negative value to a positive one
+        value = value * -1;
+    }
+    else // zero speed
+    {
+        // Cut power to the motor
+        pMotor_->run(RELEASE);
+    }
+
+    pMotor_->setSpeed(value);
+}
