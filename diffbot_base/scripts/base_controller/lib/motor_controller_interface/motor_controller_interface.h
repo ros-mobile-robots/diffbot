6fffe63108fd9bce8697077363cf2fd83fbefd18
Franz Pucher <franz.pucher@gmail.com>
2021-05-30 22:29:15 +0200

    add abstract MotorControllerInterface class

---
+++ diffbot_base/scripts/base_controller/lib/motor_controller_interface/motor_controller_interface.h
@@ -0,0 +1,30 @@
+#ifndef MOTOR_CONTROLLER_INTF_H
+#define MOTOR_CONTROLLER_INTF_H
+
+/** \brief Abstract base interface class for a motor controller
+ * 
+ * Inherit from this base class and specify the type of 
+ * motor driver. The interface provides setSpeed, which
+ * is an abstract method and therefore must be implemented.
+ */
+
+template<typename TMotorDriver>
+class MotorControllerIntf
+{
+    public:
+        /** \brief Set the speed of the a motor
+         * 
+         * Implement this method to set the speed of a 
+         * motor that is connected to the \code motor_driver_
+         * which is of type TMotorDriver.
+         * 
+         * \param value positive or negative value to set the direction
+         * and speed of the motor.
+         */
+        virtual void setSpeed(int value) = 0;
+
+    protected:
+        TMotorDriver motor_driver_;
+};
+
+#endif // MOTOR_CONTROLLER_INTF_H
