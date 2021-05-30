32b61d89092e5f6fc369ef533d6c47507029a50e
Franz Pucher <franz.pucher@gmail.com>
2021-05-30 21:37:02 +0200

    update encoder lib for rosserial
    - add doc strings to encoder.h
    - add ticksToAngle method
    - add angularVelocity method

--- diffbot_base/scripts/base_controller/lib/encoder/encoder.cpp
+++ diffbot_base/scripts/base_controller/lib/encoder/encoder.cpp
@@ -1,10 +1,42 @@
 #include "encoder.h"
 
 
-diffbot::Encoder::Encoder(uint8_t pin1, uint8_t pin2)
+diffbot::Encoder::Encoder(uint8_t pin1, uint8_t pin2, int encoder_resolution)
   : encoder(pin1, pin2)
+  , encoder_resolution_(encoder_resolution)
+  , prev_update_time_(0)
+  , prev_encoder_ticks_(0)
 {
 
+}
+
+
+float diffbot::Encoder::angularVelocity()
+{
+    long encoder_ticks = encoder.read();
+    //this function calculates the motor's rotational (angular) velocity based on encoder ticks and delta time
+    unsigned long current_time = millis();
+    unsigned long dt = current_time - prev_update_time_;
+
+    //convert the time from milliseconds to seconds
+    double dts = (double)dt / 1000;
+
+    //calculate wheel's speed (RPM)
+    double delta_ticks = encoder_ticks - prev_encoder_ticks_;
+    double delta_angle = ticksToAngle(delta_ticks);
+    double angular_velocity = delta_angle / dts;
+
+    prev_update_time_ = current_time;
+    prev_encoder_ticks_ = encoder_ticks;
+
+    return angular_velocity;
+}
+
+double diffbot::Encoder::ticksToAngle(const int &ticks) const
+{
+  // Convert number of encoder ticks to angle in radians
+  double angle = (double)ticks * (2.0*M_PI / encoder_resolution_);
+  return angle;
 }
 
 
@@ -24,5 +56,5 @@ int diffbot::Encoder::getRPM()
     prev_update_time_ = current_time;
     prev_encoder_ticks_ = encoder_ticks;
 
-    return (delta_ticks / counts_per_rev_) / dtm;
+    return (delta_ticks / encoder_resolution_) / dtm;
 }
--- diffbot_base/scripts/base_controller/lib/encoder/encoder.h
+++ diffbot_base/scripts/base_controller/lib/encoder/encoder.h
@@ -8,15 +8,48 @@ namespace diffbot
     public:
         ::Encoder encoder;
 
-        Encoder(uint8_t pin1, uint8_t pin2);
+        Encoder(uint8_t pin1, uint8_t pin2, int encoder_resolution);
 
+        /** \brief get revolutions per minute
+         *
+         * Calculates the wheels revolution per minute using the encoder ticks.
+         * 
+         * Based on https://github.com/linorobot/linorobot/blob/53bc4abf150632fbc6c28cdfcc207caa0f81d2b1/teensy/firmware/lib/encoder/Encoder.h
+         * 
+         * \returns revolutions per minute
+         */
         int getRPM();
 
+        /** \brief Get the measure the angular joint velocity
+         *
+         * Calculates the angular velocity of the wheel joint using the encoder ticks.
+         * 
+         * \returns angular wheel joint velocity (rad/s)
+         */
+        float angularVelocity();
+
+        /** \brief Convert number of encoder ticks to angle in radians 
+         *
+         * Calculates the current tick count of the encoder to its absolute angle in radians.
+         * 
+         * \returns angle corresponding to encoder ticks (rad)
+         */
+        double ticksToAngle(const int &ticks) const;
+
+        /** \brief Read the current encoder tick count
+         * 
+         * \returns encoder ticks
+         */
         inline int32_t read() { return encoder.read(); };
+
+        /** \brief Set the encoder tick count
+         * 
+         * \param p encoder ticks
+         */
         inline void write(int32_t p) { encoder.write(p); };
 
     private:
-        int counts_per_rev_;
+        int encoder_resolution_;
         unsigned long prev_update_time_;
         long prev_encoder_ticks_;
     };
