29a6ecab2a8d13660f897ca356d644f5d07fc6d2
Franz Pucher <franz.pucher@gmail.com>
2021-06-02 23:08:01 +0200

    add encoder resolution to config header

--- diffbot_base/scripts/base_controller/lib/config/diffbot_base_config.h
+++ diffbot_base/scripts/base_controller/lib/config/diffbot_base_config.h
@@ -3,6 +3,9 @@
 /// Encoder pins
 #define ENCODER_LEFT_H1 5
 #define ENCODER_LEFT_H2 6
+// Encoder resolution used for initialization 
+// will be read from parameter server
+#define ENCODER_RESOLUTION 542
 
 #define ENCODER_RIGHT_H1 7
 #define ENCODER_RIGHT_H2 8
