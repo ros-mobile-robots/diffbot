54e130b464da3ce3a51709f9dd1c6aef03eed598
Franz Pucher <franz.pucher@gmail.com>
2022-01-16 21:39:29 +0100

    fix: test_encoders.cpp include name

--- diffbot_base/scripts/base_controller/test/encoders/test_encoders.cpp
+++ diffbot_base/scripts/base_controller/test/encoders/test_encoders.cpp
@@ -5,31 +5,38 @@
  * Turn the wheels by hand and check the output in the serial monitor.
  */
 
+// Include unity library
+// For details see https://docs.platformio.org/en/latest/plus/unit-testing.html#api
 #include <unity.h>
 
+#include <ros.h>
 #include "diffbot_base_config.h"
-#include "encoder.h"
+#include "encoder_diffbot.h"
 
+
+// ROS node handle
+ros::NodeHandle nh;
 
 // Encoder setup
 // Change these pin numbers to the pins connected to your encoder.
 //   Best Performance: both pins have interrupt capability
 //   Good Performance: only the first pin has interrupt capability
 //   Low Performance:  neither pin has interrupt capability
-diffbot::Encoder encoderLeft(ENCODER_LEFT_H1, ENCODER_LEFT_H2);  // Default pins 5, 6
-diffbot::Encoder encoderRight(ENCODER_RIGHT_H1, ENCODER_RIGHT_H2); // Default pins 7, 8
+diffbot::Encoder encoderLeft(nh, ENCODER_LEFT_H1, ENCODER_LEFT_H2, ENCODER_RESOLUTION);  // Default pins 5, 6
+diffbot::Encoder encoderRight(nh, ENCODER_RIGHT_H1, ENCODER_RIGHT_H2, ENCODER_RESOLUTION); // Default pins 7, 8
 //   avoid using pins with LEDs attached
 
 void setup() {
   Serial.begin(115200);
   Serial.println("DiffBot Wheel Encoders:");
+  Serial.println("Trun wheels by hand to check for correct encoder output.");
 }
 
 long positionLeft  = -999;
 long positionRight = -999;
 
 int test_start_time = millis();
-int test_max_time = 5000;
+int test_max_time = 8000;
 
 void loop() {
   UNITY_BEGIN();
